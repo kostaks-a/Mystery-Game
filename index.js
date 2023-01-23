e70d7518a4a4f2c5ea4ed47e5ae23e8b803ab3e6
“Konstantinos <kostaks@outlook.com>
2023-01-23 23:04:49 +0100

    latest update

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ eMartini.src = "/images/e-martini.png"
 
 const drinksArray = []
 drinksArray.push(water, cola, beer, eMartini)
+console.log (drinksArray)
 
 
 class drinks {
@@ -32,43 +33,71 @@ class drinks {
 ////////////////////////////
 let animatedId
 let frames = 0
+let score = 0
+let drinksTotal = 0
+let alcohol = 0
+let randomDrink = ``
 
+//document.querySelector('#registration-page').style.display = "none"
 
-document.querySelector('#registration-page').style.display = "none"
-
-
-
-document.querySelector('#game-screen').style.display = "none"
+//document.querySelector('#game-screen').style.display = "none"
 
 function registerPage(){
     document.querySelector('#registration-page').style.display = "block" 
 }
 
+function reveal(){
+  document.getElementById('drink-button').onclick = () => {
+  randomDrink = drinksArray[Math.floor(Math.random() * drinksArray.length)];
+  console.log (randomDrink)
+  drinksArray.forEach(drink => {
+    if (randomDrink === drink){
+      ctx.drawImage(randomDrink , 150 , 80 , 40 , 35)
+    }
+  });
+  }
+  if (randomDrink.includes("beer")){
+    console.log (`yeah`)
+  }
+ switch (randomDrink) {
+  case `beer`: console.log (`yeah`)
+  break;
+  case `water`: console.log (`oh shit`)
+  break;
+  case `eMartini`: console.log (`fuck yea`)
+  break;
+  case `cola` : console.log (`meh`);
+ }
+  
+}
+
 //////// REGISTRATION PAGE ////
 
-window.onload = () => {
+/*window.onload = () => {
     document.getElementById('enter-button').onclick = () => {
     document.querySelector('.game-intro').style.display = "none"
     
   registerPage();
   };
 }
+*/
 
 ///////GAME START PAGE ///////
 function updateDrinks(){
   frames++
-  if (frames % 200 === 0){
+  if (frames % 100 === 0){
    // console.log("whatever")
-    ctx.drawImage( eMartini , 125 , 65 , 30 , 30)
+    ctx.drawImage( mysteryBox , 125 , 65 , 30 , 30)
   }
-  
-
+    //console.log (score , alcohol)
+    
 }
-
+reveal()
 
 function animate(){
   //ctx.drawImage(mysteryBox , 150 , 80 , 40 , 35)
   updateDrinks()
+
 
   animatedId = requestAnimationFrame(animate)
 }
@@ -77,7 +106,7 @@ function animate(){
 
 function startGame(){
   
-  document.querySelector('#registration-page #start-button').parentElement.remove()
+  // document.querySelector('#registration-page #start-button').parentElement.remove()
   document.querySelector('#game-screen').style.display = "block"
   animate()
 
@@ -88,3 +117,4 @@ function startGame(){
 document.getElementById('start-button').onclick = () => {
       startGame();
     };
+
