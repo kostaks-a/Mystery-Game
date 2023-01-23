b9d342f2208f70ad463955b01f8cf3a074edf85a
“Konstantinos <kostaks@outlook.com>
2023-01-23 19:54:01 +0100

    first and second screen close to be finalized

--- index.js
+++ index.js
@@ -1,26 +1,90 @@
-const myCanvas = document.querySelector('canvas')
-const ctx = myCanvas.getContext('2d')
-document.querySelector('#registration-page').style.visibility = "hidden"
+myCanvas = document.querySelector('canvas')
+ctx = myCanvas.getContext('2d')
+
+///// Drinks img init/////
+const mysteryBox = new Image()
+mysteryBox.src =  "/images/box.png"
+
+const water = new Image()
+water.src = "/images/water.png"
+
+const cola = new Image()
+cola.src = "/images/cola 1.png"
+
+const beer = new Image()
+beer.src = "/images/beer.png"
+
+const eMartini = new Image()
+eMartini.src = "/images/e-martini.png"
+
+const drinksArray = []
+drinksArray.push(water, cola, beer, eMartini)
+
+
+class drinks {
+  constructor (width, height , x , y){
+    this.width = width
+    this.height = height
+    this.x = x
+    this.y = y
+  }
+}
+////////////////////////////
+let animatedId
+let frames = 0
+
+
+document.querySelector('#registration-page').style.display = "none"
+
+
+
+document.querySelector('#game-screen').style.display = "none"
 
 function registerPage(){
-    document.querySelector('#registration-page').style.visibility = "visible"  
+    document.querySelector('#registration-page').style.display = "block" 
 }
 
-//// START GAME -> REGISTRATION PAGE ////
+//////// REGISTRATION PAGE ////
 
 window.onload = () => {
     document.getElementById('enter-button').onclick = () => {
-    console.log ('paok')
-    const parent = document.querySelector('.game-intro #enter-button')
-    parent.parentElement.remove();
-    registerPage();
+    document.querySelector('.game-intro').style.display = "none"
+    
+  registerPage();
   };
+}
+
+///////GAME START PAGE ///////
+function updateDrinks(){
+  frames++
+  if (frames % 200 === 0){
+   // console.log("whatever")
+    ctx.drawImage( eMartini , 125 , 65 , 30 , 30)
+  }
+  
+
+}
+
+
+function animate(){
+  //ctx.drawImage(mysteryBox , 150 , 80 , 40 , 35)
+  updateDrinks()
+
+  animatedId = requestAnimationFrame(animate)
 }
 
 
 
-/*window.onload = () => {
-    document.getElementById('ready-button').onclick = () => {
-      startGame;
+function startGame(){
+  
+  document.querySelector('#registration-page #start-button').parentElement.remove()
+  document.querySelector('#game-screen').style.display = "block"
+  animate()
+
+
+}
+
+
+document.getElementById('start-button').onclick = () => {
+      startGame();
     };
-}*/
